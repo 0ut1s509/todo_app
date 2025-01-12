0b20d000ca7aaf947942a8a2960259312a999107
0ut1s509 <gentlemansondy509@gmail.com>
2025-01-12 13:34:29 -0500

    listview

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Task
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+class Tasklist(ListView):
+    model = Task
+    context_object_name = 'tasks' #spesifye non model la nan template la
+    
