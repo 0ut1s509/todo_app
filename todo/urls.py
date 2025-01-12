9390830bb9fb550ed261414c106960caed89dfdc
0ut1s509 <gentlemansondy509@gmail.com>
2025-01-12 14:12:06 -0500

    update view

--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('tasks/',views.Tasklist.as_view(),name='tasks'),
     path('task/<int:pk>/',views.TaskDetail.as_view(),name='task'),
     path('task/create/', views.TaskCreate.as_view(),name='task-create'),
+    path('task/update/<int:pk>/', views.TaskUpdate.as_view(),name='task-update'),
     
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Task
 from django.urls import reverse_lazy
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -27,3 +28,12 @@ class TaskCreate(CreateView):
         form.instance.user = self.request.user
         messages.success(self.request, "The task was created successfully.")
         return super(TaskCreate,self).form_valid(form)
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title','description','completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was updated successfully.")
+        return super(TaskUpdate,self).form_valid(form)
