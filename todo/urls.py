ca1607990392b5d8644f0f8b19ef0f90baadda44
0ut1s509 <gentlemansondy509@gmail.com>
2025-01-12 14:21:09 -0500

    update view

--- todo/urls.py
+++ todo/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('task/<int:pk>/',views.TaskDetail.as_view(),name='task'),
     path('task/create/', views.TaskCreate.as_view(),name='task-create'),
     path('task/update/<int:pk>/', views.TaskUpdate.as_view(),name='task-update'),
+    path('task/delete/<int:pk>/', views.TaskDelete.as_view(),name='task-delete'),
     
 
 ]
