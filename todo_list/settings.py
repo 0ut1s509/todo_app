eaeb7161272d3dab6f0c5f177b92f3228ec007be
0ut1s509 <gentlemansondy509@gmail.com>
2025-01-12 15:58:37 -0500

    password reset

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
